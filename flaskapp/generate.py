046387896b6ca1c210e1f4a8e7b524809eebb309
udaygoyat45 <20959465+udaygoyat45@users.noreply.github.com>
2020-01-10 22:13:55 -0500

    formatting the code

--- flaskapp/generate.py
+++ flaskapp/generate.py
@@ -1,3 +1,4 @@
-def generate_id (N):
-    import random, string
+def generate_id(N):
+    import random
+    import string
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
