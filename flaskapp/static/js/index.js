85045cd0ef06af8ca4fde320e078393f3c95a644
Uday Goyat <udaygoyat45@gmail.com>
2020-05-22 16:31:03 -0400

    bruh

--- flaskapp/static/js/index.js
+++ flaskapp/static/js/index.js
@@ -1,24 +1,17 @@
 $(document).ready(function () {
-    console.log("js is ready");
-
-    $(".jumbotron .container .overpass, #slogan, .navbar-inner, #extra-content, #title, #second, #third, .carousel, #footer").hide();
-    
+    $("#footer, .jumbotron .container .overpass, #slogan, .navbar-inner, #extra-content, #title, #second, #third, .carousel, #footer").hide();
     $("#title").fadeIn();
     $(".jumbotron .container .overpass").fadeIn(500, function () {
         $(".navbar-inner").slideDown().delay(1000);
         $("#extra-content").fadeIn().delay();
         $("#slogan").slideDown(1000, function () {
-            
-            $("#third").slideDown(1000, function() {
-
-            });
+            $("#third").slideDown(1000);
             $("#second").slideDown(1000, function () {
                 $(".carousel, #footer").show();
+                $("#footer").show();
             });
         });
     });
-
-    console.log("js is done");
 });
 
 $("#footer-collapse").click(function() {
